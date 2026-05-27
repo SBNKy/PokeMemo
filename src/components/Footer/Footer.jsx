0ad902b226db0b34fb686201e8ff668c3e0d9079
Sebastian Konieczny <s.konieczny3211@gmail.com>
2026-05-27 13:04:11 +0200

    feat: add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,18 @@
+import styles from "./Footer.module.css";
+import githubIcon from "../../assets/icons/github.png";
+
 export default function Footer() {
-    return <footer></footer>;
+    return (
+        <footer>
+            <a
+                href="https://github.com/SBNKy/PokeMemo"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.footerLink}
+            >
+                <img src={githubIcon} alt="Github icon" height={20} />
+                <span>Created by SBNKy</span>
+            </a>
+        </footer>
+    );
 }
